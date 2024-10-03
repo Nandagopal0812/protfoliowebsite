96fb2a004d71c1a85d9ec5ff11901f9e64e3adef
Nandagopal0812 <Gopalnanda701@gmail.com>
2024-10-03 13:27:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "BCA", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+function sub(x , y) {
+   return x * y ;
+}
+console.log(sub(20,30));
+
+function multiple(a , b) {
+  return a - b ;
+}
+console.log(multiple(40 ,70));
+
+
